a091155629b59809fe8640bce6f4a6ea378fd1d6
Tarek <tarekmohamedd8@gmail.com>
2025-08-09 13:38:19 -0400

    Initial commit (without large zip)

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'amirdzboukhors',
+    'purchase_key' => '9ad1d701-5632-4f0b-ac83-25599a15d921',
+    'software_id' => 'MzE0NDg1OTc=',
+    'domain' => 'sharqia.kboom.center',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '36085041',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'theme_lifestyle' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
