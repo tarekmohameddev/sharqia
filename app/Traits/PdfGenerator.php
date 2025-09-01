18010af69f5c06b30bce43cfb0494d185c3429a6
Tarek <tarekmohamedd8@gmail.com>
2025-09-01 10:23:33 +0300

    Remove footer HTML setting for invoice type in PdfGenerator trait

--- app/Traits/PdfGenerator.php
+++ app/Traits/PdfGenerator.php
@@ -22,10 +22,6 @@ trait  PdfGenerator
         $mpdf = new \Mpdf\Mpdf(['default_font' => 'FreeSerif', 'mode' => 'utf-8', 'format' => [190, 250], 'autoLangToFont' => true]);
         $mpdf->autoScriptToLang = true;
         $mpdf->autoLangToFont = true;
-        if ($pdfType = 'invoice') {
-            $footerHtml = self::footerHtml($requestFrom);
-            $mpdf->SetHTMLFooter($footerHtml);
-        }
         $mpdf_view = $view;
         $mpdf_view = $mpdf_view->render();
         $mpdf->WriteHTML($mpdf_view);
