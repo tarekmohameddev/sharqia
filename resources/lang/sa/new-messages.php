c5e1e458ee022d35c0f1639dd825d9d3c1710081
Tarek <tarekmohamedd8@gmail.com>
2025-09-20 07:21:09 -0400

    fix conff

--- resources/lang/sa/new-messages.php
+++ resources/lang/sa/new-messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+	"all_messages_are_in_translated" => "All messages are in translated",
 	"want_to_remove_country_name?" => "Want to remove country name ",
 	"want_to_clear_all_stock_clearance_products?" => "Want to clear all stock clearance products ",
 	"alternative_phone" => "Alternative phone",
